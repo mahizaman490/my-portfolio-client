edb3f21273fed727821602d03e8cbce049624e99
Mahi Zaman <mahizaman490@gmail.com>
2023-12-11 10:05:35 +0600

    updated some links

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -8,16 +8,16 @@ const Contact = () => {
       <h1 className="text-3xl font-bold mb-6 text-center text-cyan-600">Contact Me</h1>
 
       <div className="flex items-center justify-center space-x-4">
-        <a href="mailto:your.email@example.com" className="text-4xl text-blue-500 hover:underline">
+        <a href="mailto:mahizaman490@gmail.com" className="text-4xl text-blue-500 hover:underline">
           <FaEnvelope />
         </a>
-        <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer" className="text-4xl text-blue-500 hover:underline">
+        <a href="https://www.linkedin.com/in/fatematuz-jannat-mahi-9315b52a0/" target="_blank" rel="noopener noreferrer" className="text-4xl text-blue-500 hover:underline">
           <FaLinkedin />
         </a>
-        <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer" className="text-4xl text-blue-500 hover:underline">
+        <a href="https://github.com/mahizaman490" target="_blank" rel="noopener noreferrer" className="text-4xl text-blue-500 hover:underline">
           <FaGithub />
         </a>
-        <a href="https://twitter.com/your-twitter-handle" target="_blank" rel="noopener noreferrer" className="text-4xl text-blue-500 hover:underline">
+        <a href="https://twitter.com/mahi3374" target="_blank" rel="noopener noreferrer" className="text-4xl text-blue-500 hover:underline">
           <FaTwitter />
         </a>
       </div>
